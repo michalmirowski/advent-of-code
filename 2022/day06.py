f2b088a4c330e9b99ac203655b8710336ae13034
Michal Mirowski <mirowskima@gmail.com>
2022-12-21 20:34:55 +0100

    Improve day 6/2022

--- 2022/day06.py
+++ 2022/day06.py
@@ -2,16 +2,21 @@
 with open('input_files/input06.txt') as puzzle_input:
     data = puzzle_input.read()
 
-marker = []
-counter = 0
-marker_len = 14  # 4 to get answer for part one, 14 for part two
 
 # part one and two solution
-for i in range(len(data)):
-    marker += data[i]
-    counter += 1
-    if len(marker) == marker_len:
-        if len(marker) == len(set(marker)):  # check if all elements in list are unique
-            break
-        del marker[0]
-print('First marker after', counter, 'characters and it is:', marker)
+def find_marker_in_datastream(datastream, unique_len):
+    marker = []
+    counter = 0
+    for i in range(len(datastream)):
+        marker += datastream[i]
+        counter += 1
+        if len(marker) == unique_len:
+            if len(marker) == len(set(marker)):  # check if all elements in list are unique
+                break
+            del marker[0]
+    print('When marker has', unique_len, 'unique characters,then first marker is after', counter,
+          'characters and it is:', marker)
+
+
+find_marker_in_datastream(data, 4)
+find_marker_in_datastream(data, 14)
